59f9f7399b4d834a83a28ca99ccbf532a79708ba
kintal2026 <38499600+DatBlueDev@users.noreply.github.com>
2023-02-21 08:50:45 -0600

    Add files via upload

---
+++ CompSciGame/gameCode.js
@@ -0,0 +1,86 @@
+var moveDisplacement = 90;
+var HoldingButton = false;
+var buttonHeld;
+var div = document.getElementById("playerCharacter");
+var gameGrid = document.getElementById("gameGrid");
+var initHoriz = +div.offsetLeft;
+var initVertic = +div.offsetTop;
+
+var bullet = document.getElementById("bullet1");
+
+gameGrid.style.left = initHoriz + "px";
+gameGrid.style.top = initVertic + "px"; 
+$(document).keydown(function(event) {
+    var key = (event.keyCode ? event.keyCode : event.which);
+
+    if (key === 65){ // A
+        var div = document.getElementById("playerCharacter");
+        
+        div.style.left = initHoriz - moveDisplacement  + "px"
+
+    }
+    
+    if (key === 68){ // D
+        var div = document.getElementById("playerCharacter");
+        div.style.left = initHoriz + moveDisplacement  + "px"
+    }
+    
+    if (key === 87){ // W
+        var div = document.getElementById("playerCharacter");
+        div.style.top =initVertic - moveDisplacement  + "px"
+    }
+    
+    if (key === 88){ // X
+        var div = document.getElementById("playerCharacter");
+        div.style.top = initVertic+ moveDisplacement  + "px"
+    }
+
+    if (key === 81){ // Q
+        var div = document.getElementById("playerCharacter");
+        div.style.left = initHoriz - moveDisplacement  + "px"
+        div.style.top = initVertic-moveDisplacement  + "px"
+    }
+
+    if (key === 69){ // E
+        var div = document.getElementById("playerCharacter");
+        div.style.left = initHoriz + moveDisplacement  + "px"
+        div.style.top = initVertic -moveDisplacement  + "px"
+    }
+    
+    if (key === 90){ // Z
+        var div = document.getElementById("playerCharacter");
+        div.style.left = initHoriz - moveDisplacement  + "px"
+        div.style.top =initVertic +moveDisplacement  + "px"   
+            }
+    
+    if (key === 67){ // C
+        var div = document.getElementById("playerCharacter");
+        div.style.left = initHoriz + moveDisplacement  + "px"
+        div.style.top = initVertic +moveDisplacement  + "px"   
+    }
+    HoldingButton = true;
+    buttonHeld = key;
+
+}
+    
+);
+$(document).keyup(function(event) {
+    var key = (event.keyCode ? event.keyCode : event.which);
+
+    var div = document.getElementById("playerCharacter");
+
+    div.style.left = initHoriz + "px";
+    div.style.top = initVertic + "px";
+
+
+}
+);
+
+setInterval(moveBullet,10);
+    
+function moveBullet(){
+    bullet.style.left = bullet.offsetLeft + 5  + "px";
+
+}
+
+gameGrid()
